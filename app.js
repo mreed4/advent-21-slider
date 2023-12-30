e706706b67d9813b93507a439f78e8dd31721f37
Matthew <78713317+mreed4@users.noreply.github.com>
2023-12-29 20:52:05 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const dollars = document.querySelector(".dollars");
+const slider = document.querySelector(".slider");
+
+slider.value = 5000; // 50 dollars
+dollars.textContent = integerToCurrency(slider.value);
+
+function integerToCurrency(value) {
+  let dollars = Math.floor(value / 100);
+  let cents = value % 100;
+  return `${leadingZero(dollars)}.${leadingZero(cents)}`;
+}
+
+function leadingZero(value) {
+  return value < 10 ? `0${value}` : value;
+}
+
+slider.addEventListener("input", () => {
+  dollars.textContent = integerToCurrency(slider.value);
+});
